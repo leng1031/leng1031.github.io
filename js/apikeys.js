1268962bdf7163a321b1f9ceccc157dcd8116b94
leng1031 <xu.leng@outlook.com>
2022-01-10 13:08:12 -0500

    portfolio add function

---
+++ js/apikeys.js
@@ -0,0 +1,4 @@
+export default {
+    USER_ID : 'user_GiEcq9z4wbAN1L1cBaAm2',
+    TEMPLATE_ID:'template_jgk3836'
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+import emailjs from 'emailjs-com'
+import apikeys from "./apikeys";
+
 let menu = document.querySelector('#menu-btn');
 let header = document.querySelector('.header');
 
@@ -15,3 +18,34 @@ themeToggler.onclick = () => {
         document.body.classList.remove('active');
     }
 }
+
+// var to = document.getElementById("formTo").value;
+// var name = document.getElementById("formName").value;
+// var subject = document.getElementById("formSubject").value;
+// var message = document.getElementById("formMessage").value;
+//
+//
+// emailjs.send("service_x93c6l9","template_jgk3836",{
+//     subject: subject,
+//     to: to,
+//     name: name,
+//     message: message,
+// });
+
+const sendEmail = e =>{
+    e.preventDefault()
+
+    emailjs
+        .sendForm('gmail',apikeys.TEMPLATE_ID,e.target,apikeys.USER_ID)
+        .then(
+            result =>{
+                console.log(result.text)
+            },
+            error =>{
+                console.log(error.text)
+            }
+        )
+}
+
+const form = document.querySelector('.form')
+form.addEventListener('submit',sendEmail)
